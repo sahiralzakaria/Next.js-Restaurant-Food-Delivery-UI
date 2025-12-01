a68a60ce4222a3756cc2823ff7295d07c4d5fd7d
sahir zakaria <sahir.alzakaria@gmail.com>
2025-12-02 01:15:00 +0300

    add responsive styles to Navbar

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image'
 import Link from 'next/link';
 import { useState } from 'react';
+import CartIcon from './CartIcon';
 
 const links = [
     { id: 1, title: 'Homepage', url: '/' },
@@ -13,18 +14,22 @@ const links = [
 
 const Menu = () => {
     const [open, setOpen] = useState(false);
+    const user = false;
     return (
         <div>
             {!open ? <Image src='/open.png' alt='' width={20} height={20} onClick={() => { setOpen(true) }} /> :
                 <Image src='/close.png' alt='' width={20} height={20} onClick={() => { setOpen(false) }} />}
 
-
-            {open && <div className='h-[calc(100vh-6rem)] w-full p-4 bg-red-500 text-white flex flex-col absolute left-0 top-24 items-center justify-center gap-8 text-3xl z-10'>  {
+            {open && (<div className='h-[calc(100vh-6rem)] w-full p-4 bg-red-500 text-white flex flex-col absolute left-0 top-24 items-center justify-center gap-8 text-3xl z-10'>  {
                 links.map(item => (
-                    <Link key={item.id} href={item.url}>{item.title}</Link>
+                    <Link key={item.id} href={item.url} onClick={() => setOpen(false)}>{item.title}</Link>
                 ))
-            }</div>}
-
+            }
+                {!user ? (<Link href='/login' onClick={() => setOpen(false)}>Login</Link>) : (<Link href='/orders' onClick={() => setOpen(false)}>Orders</Link>)}
+                <Link href='/cart' onClick={() => setOpen(false)}>
+                    <CartIcon />
+                </Link>
+            </div>)}
         </div>
     )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,37 @@
 import Link from 'next/link'
-import React from 'react'
 import Menu from './Menu'
+import CartIcon from './CartIcon';
+import Image from 'next/image';
 
 const Navbar = () => {
+
+    const user = false;
     return (
-        <div className='h-12 p-4 text-red-500 flex justify-between border-b-2 border-b-red-500 uppercase'>
+        <div className='h-12 p-4 text-red-500 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40'>
+
+            {/* LEFT LINKS */}
+            <div className='hidden md:flex gap-4 flex-1'>
+                <Link href='/'>HomePage</Link>
+                <Link href='/menu'>Menu</Link>
+                <Link href='/'>Contact</Link>
+            </div>
             {/* LOGO */}
-            <div className='text-xl'>
+            <div className='text-xl md:font-bold flex-1 md:text-center'>
                 <Link href='/'>Massimo</Link>
             </div>
             {/* MOBILE MENU */}
-            <div>
+            <div className='md:hidden'>
                 <Menu />
+            </div>
+            {/* RIGHT LINKS */}
+            <div className='hidden md:flex gap-4 items-center justify-end flex-1'>
+                <div className='md:absolute top-3 right-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1  justify-center rounded-md '>
+                    <Image src='/phone.png' alt='' width={20} height={20} />
+                    <span>123 765 45 54</span>
+                </div>
+                {!user ? (<Link href='/login'>Login</Link>) :
+                    (<Link href='/orders'>Orders</Link>)}
+                <CartIcon />
             </div>
         </div>
     )
