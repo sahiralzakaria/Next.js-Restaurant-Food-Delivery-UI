714198e74207101d4b35acc9cef6443f972d803f
sahir zakaria <sahir.alzakaria@gmail.com>
2025-12-01 15:30:31 +0300

    add components to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Notification from "@/components/Notification";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Notification />
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/Notification.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notification = () => {
+    return (
+        <div className='h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>Enjoy free shipping on orders above $50. Get your meal delivered today!</div>
+
+
+    )
+}
+
+export default Notification
