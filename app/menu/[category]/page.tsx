f7c9922ea5b8b114f925705aa62741f5dcbcb508
sahir zakaria <sahir.alzakaria@gmail.com>
2025-12-05 01:08:21 +0300

    finish Product List Page Design

--- app/menu/[category]/page.tsx
+++ app/menu/[category]/page.tsx
@@ -6,13 +6,18 @@ const CategoryPage = () => {
     return (
         <div className="flex flex-wrap text-red-500 ">
             {pizzas.map(item => (
-                <Link className='w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4' href={`/product/${item.id}`} key={item.id} >
+                <Link className='w-full h-[60vh] border-r-2 border-b-2 border-red-500 even:bg-fuchsia-50 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group' href={`/product/${item.id}`} key={item.id} >
                     {/* IMAGE CONTAINER */}
                     <div className="relative h-[80%]">
                         {item.img && <Image src={item.img} alt="" fill className="object-contain" />}
                     </div>
 
                     {/* TEXT CONTAINER */}
+                    <div className="flex items-center justify-between font-bold">
+                        <h1 className="text-2xl uppercase p-2">{item.title}</h1>
+                        <h2 className="group-hover:hidden text-xl">${item.price}</h2>
+                        <button className="hidden uppercase bg-red-500 text-white p-2 rounded-md group-hover:block">Add to Cart</button>
+                    </div>
                 </Link>
             ))
             }
