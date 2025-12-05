2d2a50f8feefb305542a58ba086a3b8340a38565
sahir zakaria <sahir.alzakaria@gmail.com>
2025-12-05 22:25:21 +0300

    add more items and styles to single product page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,18 +1,17 @@
 import Price from '@/components/Price'
 import { singleProduct } from '@/data'
 import Image from 'next/image'
-import React from 'react'
 
 const SingleProductPage = () => {
     return (
         <div className='p-4 lg:p-20 xl:p-40 h-screen flex-col justify-around text-red-500 md:flex-row'>
             {/* IMAGE CONTAINER */}
-            {singleProduct.img && (<div className='relative'>
+            {singleProduct.img && <div className='relative w-full h-1/2'>
                 <Image src={singleProduct.img} alt='' fill className='object-contain' />
-            </div>)}
+            </div>}
             {/* TEXT CONTAINER */}
-            <div className='w-1/2'>
-                <h1>{singleProduct.title}</h1>
+            <div className='h-1/2 flex flex-col gap-4'>
+                <h1 className='text-3xl font-bold uppercase'>{singleProduct.title}</h1>
                 <p>{singleProduct.desc}</p>
                 <Price price={singleProduct.price} id={singleProduct.id} options={singleProduct.options} />
             </div>
--- components/Price.tsx
+++ components/Price.tsx
@@ -1,4 +1,4 @@
-"use client"
+// "use client"
 
 type Props = {
     price: number;
@@ -8,12 +8,28 @@ type Props = {
 }
 const Price = ({ price, id, options }: Props) => {
     return (
-        <div>
-            <h2>${price.toFixed(2)}</h2>
+        <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold">${price.toFixed(2)}</h2>
             {/* OPTIONS CONTAINER */}
-            <div className=""></div>
+            <div className="flex gap-4">
+                {options?.map(option => (
+                    <button className="p-2 ring-1 ring-red-400 rounded-md" key={option.title}>{option.title}</button>
+                ))}
+            </div>
             {/* QUANTITY AND ADD BUTTON CONTAINER */}
-            <div className=""></div>
+            <div className="flex justify-between items-center">
+                {/* QUANTITY */}
+                <div className="flex justify-between w-full p-3 ring-1 ring-red-500">
+                    <span className="">Quantity</span>
+                    <div className="flex gap-4 items-center">
+                        <button>{'<'}</button>
+                        <span>1</span>
+                        <button>{'>'}</button>
+                    </div>
+                </div>
+                {/* CART BUTTON */}
+                <button className="uppercase w-56 bg-red-500 text-white p-3 ring-1 ring-red-500">Add to Cart</button>
+            </div>
         </div>
     )
 }
